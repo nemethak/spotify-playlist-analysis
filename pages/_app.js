992bb25699a3c97aaedefa05ae116806a42734cc
Anna Németh <cuewak@inf.elte.hu>
2022-05-04 19:08:22 +0200

    Progress bar between changing pages.

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import {SessionProvider} from 'next-auth/react';
+import NextNProgress from "nextjs-progressbar";
 import '../styles/globals.css';
 
 export default function App({Component, pageProps: {session, ...pageProps}}) {
   return (
     <SessionProvider session={session}>
+      <NextNProgress options={{ showSpinner: false }}/>
       <Component {...pageProps} />
     </SessionProvider>
   );
