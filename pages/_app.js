8da19e2da5a985e7521c8abf42af37140423a217
Anna Németh <cuewak@inf.elte.hu>
2022-05-21 22:24:46 +0200

    Fixed typo

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ export default function App({Component, pageProps: {session, ...pageProps}}) {
     <div>
       <Head>
         <title>Home - Playlist Stats for Spotify</title>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <SessionProvider session={session}>
